cf8b6a174e5b42c7ca1af3074252469f0d3fc337
J2ghz <j2.00ghz@gmail.com>
2020-05-31 00:24:35 +0200

    bench bith files

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,43 +1,62 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 use sdr_heatmap::{open_file, preprocess, preprocess_iter, preprocess_par_iter};
-use std::{
-    io::{Cursor, Read},
-    time::Duration,
-};
+use std::io::{Cursor, Read};
 
-fn read_file_to_memory() -> std::boxed::Box<std::io::Cursor<std::vec::Vec<u8>>> {
+fn read_file_to_memory(filename: &str) -> std::boxed::Box<std::io::Cursor<std::vec::Vec<u8>>> {
     let mut buf = Vec::new();
-    let mut file = open_file("samples/bench1.csv.gz");
+    let mut file = open_file(filename);
     let _ = file.read_to_end(&mut buf);
 
     Box::new(Cursor::new(buf))
 }
 
+fn get_file_size(filename: &str) -> u64 {
+    let mut file = open_file(filename);
+    let mut length: u64 = 0;
+    let mut buf = Vec::with_capacity(1024);
+    loop {
+        let read = file.read(&mut buf).unwrap();
+        if read == 0 {
+            break;
+        }
+        length += read as u64;
+    }
+    length
+}
+
 fn preprocess_bench(c: &mut Criterion) {
     let mut group = c.benchmark_group("preprocess implementations");
-
-    group.throughput(Throughput::Bytes(46474948));
-    group.measurement_time(Duration::from_secs(60));
-    group.sample_size(20);
-
-    group.bench_function("basic", |b| {
-        b.iter_with_large_setup(read_file_to_memory, |data| {
-            let summary = preprocess(data);
-            black_box(summary);
-        })
-    });
-    group.bench_function("iterator", |b| {
-        b.iter_with_large_setup(read_file_to_memory, |data| {
-            let summary = preprocess_iter(data);
-            black_box(summary);
-        })
-    });
-    group.bench_function("par_iterator", |b| {
-        b.iter_with_large_setup(read_file_to_memory, |data| {
-            let summary = preprocess_par_iter(data);
-            black_box(summary);
-        })
-    });
+    for file in ["samples/bench1.csv.gz", "samples/sample1.csv.gz"].iter() {
+        group.throughput(Throughput::Bytes(get_file_size(file)));
+        group.sample_size(10);
+        group.bench_with_input(BenchmarkId::new("basic", file), file, |b, file| {
+            b.iter_with_large_setup(
+                || read_file_to_memory(file),
+                |data| {
+                    let summary = preprocess(data);
+                    black_box(summary);
+                },
+            )
+        });
+        group.bench_with_input(BenchmarkId::new("iterator", file), file, |b, file| {
+            b.iter_with_large_setup(
+                || read_file_to_memory(file),
+                |data| {
+                    let summary = preprocess_iter(data);
+                    black_box(summary);
+                },
+            )
+        });
+        group.bench_with_input(BenchmarkId::new("par_iterator", file), file, |b, file| {
+            b.iter_with_large_setup(
+                || read_file_to_memory(file),
+                |data| {
+                    let summary = preprocess_par_iter(data);
+                    black_box(summary);
+                },
+            )
+        });
+    }
 
     group.finish();
 }
