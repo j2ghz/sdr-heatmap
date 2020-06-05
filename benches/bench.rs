25e8049607f25b9784a680b022ba2da1003516d7
J2ghz <j2.00ghz@gmail.com>
2020-06-05 23:00:01 +0200

    change to path

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,8 +1,11 @@
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 use sdr_heatmap::{open_file, preprocess, preprocess_iter, preprocess_par_iter};
-use std::io::{Cursor, Read};
+use std::{
+    io::{Cursor, Read},
+    path::{Path, PathBuf},
+};
 
-fn read_file_to_memory(filename: &str) -> std::boxed::Box<std::io::Cursor<std::vec::Vec<u8>>> {
+fn read_file_to_memory(filename: &Path) -> std::boxed::Box<std::io::Cursor<std::vec::Vec<u8>>> {
     let mut buf = Vec::new();
     let mut file = open_file(filename);
     let _ = file.read_to_end(&mut buf);
@@ -10,7 +13,7 @@ fn read_file_to_memory(filename: &str) -> std::boxed::Box<std::io::Cursor<std::v
     Box::new(Cursor::new(buf))
 }
 
-fn get_file_size(filename: &str) -> u64 {
+fn get_file_size(filename: &Path) -> u64 {
     let mut file = open_file(filename);
     let mut length: u64 = 0;
     let mut buf = vec![0; 1024];
@@ -27,7 +30,7 @@ fn get_file_size(filename: &str) -> u64 {
 
 fn preprocess_bench(c: &mut Criterion) {
     let mut group = c.benchmark_group("preprocess implementations");
-    for file in [
+    let files = [
         "samples/test0.csv.gz",
         "samples/test1.csv.gz",
         "samples/test2.csv.gz",
@@ -37,37 +40,51 @@ fn preprocess_bench(c: &mut Criterion) {
         "samples/bench1.csv.gz",
     ]
     .iter()
-    {
-        let size = get_file_size(file);
+    .map(PathBuf::from)
+    .collect::<Vec<_>>();
+    for file in files.iter() {
+        let size = get_file_size(&file);
         group.sample_size(10);
         group.throughput(Throughput::Bytes(size));
-        group.bench_with_input(BenchmarkId::new("basic", file), file, |b, file| {
-            b.iter_with_large_setup(
-                || read_file_to_memory(file),
-                |data| {
-                    let summary = preprocess(data);
-                    black_box(summary);
-                },
-            )
-        });
-        group.bench_with_input(BenchmarkId::new("iterator", file), file, |b, file| {
-            b.iter_with_large_setup(
-                || read_file_to_memory(file),
-                |data| {
-                    let summary = preprocess_iter(data);
-                    black_box(summary);
-                },
-            )
-        });
-        group.bench_with_input(BenchmarkId::new("par_iterator", file), file, |b, file| {
-            b.iter_with_large_setup(
-                || read_file_to_memory(file),
-                |data| {
-                    let summary = preprocess_par_iter(data);
-                    black_box(summary);
-                },
-            )
-        });
+        group.bench_with_input(
+            BenchmarkId::new("basic", file.display()),
+            &file,
+            |b, file| {
+                b.iter_with_large_setup(
+                    || read_file_to_memory(file),
+                    |data| {
+                        let summary = preprocess(data);
+                        black_box(summary);
+                    },
+                )
+            },
+        );
+        group.bench_with_input(
+            BenchmarkId::new("iterator", file.display()),
+            &file,
+            |b, file| {
+                b.iter_with_large_setup(
+                    || read_file_to_memory(file),
+                    |data| {
+                        let summary = preprocess_iter(data);
+                        black_box(summary);
+                    },
+                )
+            },
+        );
+        group.bench_with_input(
+            BenchmarkId::new("par_iterator", file.display()),
+            &file,
+            |b, file| {
+                b.iter_with_large_setup(
+                    || read_file_to_memory(file),
+                    |data| {
+                        let summary = preprocess_par_iter(data);
+                        black_box(summary);
+                    },
+                )
+            },
+        );
     }
 
     group.finish();
