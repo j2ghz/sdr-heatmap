40c728de8e4dc6c6bd6b43c053e325e7bea06542
J2ghz <j2.00ghz@gmail.com>
2020-08-12 23:58:44 +0200

    change palette architecture

---
+++ src/palettes/default.rs
@@ -0,0 +1,15 @@
+use super::{rescale_value_to, PaletteColorize};
+
+pub struct DefaultPalette {}
+impl PaletteColorize for DefaultPalette {
+    fn get_color(&self, value: f32) -> [u8; 3] {
+        let value = rescale_value_to(value, 0.0, 255.0) as u8;
+        [value, value, 50]
+    }
+    fn get_color_under_range(&self) -> [u8; 3] {
+        [0, 0, 0]
+    }
+    fn get_color_over_range(&self) -> [u8; 3] {
+        [255, 255, 255]
+    }
+}
