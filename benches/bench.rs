9ba580f140594d01c17bee46a487e5e711a3d9f2
J2ghz <j2.00ghz@gmail.com>
2020-08-14 22:28:36 +0200

    Add test for each file

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,5 +1,7 @@
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
-use sdr_heatmap::{open_file, preprocess, preprocess_iter, preprocess_par_iter};
+use sdr_heatmap::{
+    open_file, preprocess, preprocess_iter, preprocess_par_iter, process, process_iter,
+};
 use std::{
     io::{Cursor, Read},
     path::{Path, PathBuf},
@@ -11,6 +13,15 @@ fn read_file_to_memory(filename: &Path) -> std::boxed::Box<std::io::Cursor<std::
     let _ = file.read_to_end(&mut buf);
 
     Box::new(Cursor::new(buf))
+}
+
+fn read_csv_to_memory(
+    filename: &Path,
+) -> csv::Reader<std::boxed::Box<std::io::Cursor<std::vec::Vec<u8>>>> {
+    let file = read_file_to_memory(filename);
+    csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_reader(file)
 }
 
 fn get_file_size(filename: &Path) -> u64 {
@@ -90,5 +101,54 @@ fn preprocess_bench(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, preprocess_bench);
-criterion_main!(benches);
+fn process_bench(c: &mut Criterion) {
+    let mut group = c.benchmark_group("process implementations");
+    let files = [
+        "samples/test0.csv.gz",
+        "samples/test1.csv.gz",
+        "samples/test2.csv.gz",
+        "samples/test3.csv.gz",
+        "samples/test4.csv.gz",
+        "samples/test5.csv.gz",
+        "samples/bench1.csv.gz",
+    ]
+    .iter()
+    .map(PathBuf::from)
+    .collect::<Vec<_>>();
+    for file in files.iter() {
+        let size = get_file_size(&file);
+        group.sample_size(10);
+        group.throughput(Throughput::Bytes(size));
+        group.bench_with_input(
+            BenchmarkId::new("basic", file.display()),
+            &file,
+            |b, file| {
+                b.iter_with_large_setup(
+                    || read_csv_to_memory(file),
+                    |data| {
+                        let summary = process(data, -1000.0, 1000.0);
+                        black_box(summary);
+                    },
+                )
+            },
+        );
+        group.bench_with_input(
+            BenchmarkId::new("iterator", file.display()),
+            &file,
+            |b, file| {
+                b.iter_with_large_setup(
+                    || read_csv_to_memory(file),
+                    |data| {
+                        let summary = process_iter(data, -1000.0, 1000.0);
+                        black_box(summary);
+                    },
+                )
+            },
+        );
+    }
+
+    group.finish();
+}
+
+criterion_group!(bench, process_bench, preprocess_bench);
+criterion_main!(bench);
