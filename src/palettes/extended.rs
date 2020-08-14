3169e07ce16c5ac3e11d508a5a6927a57fc3966f
J2ghz <j2.00ghz@gmail.com>
2020-08-14 21:07:14 +0200

    format

--- src/palettes/extended.rs
+++ src/palettes/extended.rs
@@ -4,15 +4,15 @@ pub struct ExtendedPalette {}
 impl PaletteColorize for ExtendedPalette {
     fn get_color(&self, value: f32) -> [u8; 3] {
         let value = rescale_value_to(value, 0.0, 255.0);
-                let whole = value as u8;
-                let fract = value.fract();
-                if fract < 0.33 {
-                    [whole, whole, 50]
-                } else if fract < 0.66 {
-                    [whole, whole + 1, 50]
-                } else {
-                    [whole + 1, whole + 1, 50]
-                }
+        let whole = value as u8;
+        let fract = value.fract();
+        if fract < 0.33 {
+            [whole, whole, 50]
+        } else if fract < 0.66 {
+            [whole, whole + 1, 50]
+        } else {
+            [whole + 1, whole + 1, 50]
+        }
     }
     fn get_color_under_range(&self) -> [u8; 3] {
         [0, 0, 0]
